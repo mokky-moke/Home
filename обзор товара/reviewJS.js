92fd57f93a8b8f47988c96418d1001d24938d5ef
mokky-moke <toxmiro123@gmail.com>
2024-09-23 20:21:26 +1000

    review product

--- обзор товара/reviewJS.js
+++ обзор товара/reviewJS.js
@@ -1,26 +1,2 @@
-const images = [
-    'C:/Users/User/Desktop/AllTheBest/Home/foto/Group1.jpg',
-    'C:/Users/User/Desktop/AllTheBest/Home/foto/Group2.jpg',
-    'C:/Users/User/Desktop/AllTheBest/Home/foto/чехол Iphone 13.jpg'
-];
 
-let currentIndex = 0;
-
-const mainImage = document.getElementById('main-image');
-const prevButton = document.getElementById('prev');
-const nextButton = document.getElementById('next');
-
-function showImage(index) {
-    mainImage.src = images[index];
-}
-
-prevButton.addEventListener('click', () => {
-    currentIndex = (currentIndex > 0) ? currentIndex - 1 : images.length - 1;
-    showImage(currentIndex);
-});
-
-nextButton.addEventListener('click', () => {
-    currentIndex = (currentIndex < images.length - 1) ? currentIndex + 1 : 0;
-    showImage(currentIndex);
-});
 
